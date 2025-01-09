0e7d5af49c635a9b6c7cf5d93fbc039fe4c9e14c
Травин Дмитрий Андреевич <DTravin@vimpelcom.ru>
2025-01-09 15:01:25 +0300

    feat: add postgre

--- infra/modules/database_pg/main.tf
+++ infra/modules/database_pg/main.tf
@@ -152,12 +152,14 @@ resource "yandex_mdb_postgresql_user" "user" {
   cluster_id = yandex_mdb_postgresql_cluster.cluster.id
   name       = var.postgresql_user_name
   password   = var.postgresql_user_password
+  conn_limit = 50
 
-
-  permission {
-    database_name = yandex_mdb_postgresql_database.db.name
-    roles         = ["ALL"]
+  settings = {
+    default_transaction_isolation = "read committed"
+    log_min_duration_statement    = 5000
   }
+
+
 }
 
 # команда, чтобы автоматом сохранить некоторые константы для коннекта
