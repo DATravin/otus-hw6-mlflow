bae3e802559fbae7869523dfaa97d6429c7d2f11
Травин Дмитрий Андреевич <DTravin@vimpelcom.ru>
2025-01-10 14:31:41 +0300

    feat: new data

--- infra/modules/mlflow/scripts/setup.sh
+++ infra/modules/mlflow/scripts/setup.sh
@@ -35,14 +35,25 @@ sudo pip install s3cmd
 sudo pip install pandas
 
 # Устанавливаем переменные
+
+log "add env variables"
+echo "DB_USER=${postgresql_user_name}" >> /home/ubuntu/.bashrc
+echo "DB_PASS=${postgresql_user_password}" >> /home/ubuntu/.bashrc
+echo "DB_HOST=${db_host_fqdn}" >> /home/ubuntu/.bashrc
+echo "DB_PORT=6432" >> /home/ubuntu/.bashrc
+echo "DB_NAME=${postgresql_database_name}" >> /home/ubuntu/.bashrc
+echo "S3_BUCKET=${s3_bucket_name}" >> /home/ubuntu/.bashrc
+echo "MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net/" >> /home/ubuntu/.bashrc
+
+log "exports"
 export HOME="/home/ubuntu"
-export DB_USER=${postgresql_user_name}
-export DB_PASS=${postgresql_user_password}
-export DB_HOST=${db_host_fqdn}
-export DB_PORT=6432
-export DB_NAME=${postgresql_database_name}
-export S3_BUCKET=${s3_bucket_name}
-export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net/
+# export DB_USER=${postgresql_user_name}
+# export DB_PASS=${postgresql_user_password}
+# export DB_HOST=${db_host_fqdn}
+# export DB_PORT=6432
+# export DB_NAME=${postgresql_database_name}
+# export S3_BUCKET=${s3_bucket_name}
+# export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net/
 
 
 # Настраиваем s3cmd как прокинуть туда переменные?
@@ -58,6 +69,12 @@ EOF
 
 chown ubuntu:ubuntu /home/ubuntu/.s3cfg
 chmod 600 /home/ubuntu/.s3cfg
+
+log "add cert"
+mkdir -p ~/.postgresql && \
+wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" \
+    --output-document ~/.postgresql/root.crt && \
+chmod 0600 ~/.postgresql/root.crt
 
 
 log "Setup completed successfully"
