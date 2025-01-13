e3c54e9c4259a8b4e3ded274cccb2a2f73513295
Травин Дмитрий Андреевич <DTravin@vimpelcom.ru>
2025-01-13 18:05:47 +0300

    fix: test py

--- src/test_simple.py
+++ src/test_simple.py
@@ -138,7 +138,7 @@ def objective(params, train_data, test_data):
 
     with mlflow.start_run():
         mlflow.log_params(params)
-        mlflow.log_metric(dct_metrics)
+        mlflow.log_metrics(dct_metrics)
 
 
     return {'loss': -auc, 'status': STATUS_OK}
