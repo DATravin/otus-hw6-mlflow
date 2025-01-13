9d580ceb4288ad3f5dbbf6d41742f390297c6def
Травин Дмитрий Андреевич <DTravin@vimpelcom.ru>
2025-01-13 15:37:48 +0300

    fix: test py

--- dags/model_fitting.py
+++ dags/model_fitting.py
@@ -138,7 +138,7 @@ with DAG(
         task_id="dp-cluster-pyspark-task",
         main_python_file_uri=f"s3a://{S3_SOURCE_BUCKET}/src/test_simple.py",
         connection_id=YC_SA_CONNECTION.conn_id,
-        args=["--bucket", S3_BUCKET_NAME,"--mlflow", MLFLOW_HOST],
+        args=["--bucket", S3_BUCKET_NAME_COLD],
         dag=ingest_dag,
     )
     # 3 этап: удаление Dataproc кластера
